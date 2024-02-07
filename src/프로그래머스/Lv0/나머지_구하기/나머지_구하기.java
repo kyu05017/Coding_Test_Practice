002035d2207d3f26488b2fe58bd8185e3781d622
kyu05017 <kyu05017@naver.com>
2024-02-07 18:20:02 +0900

    프로그래머스 Lv0 - 나머지 구하기

---
+++ src/프로그래머스/Lv0/나머지_구하기/나머지_구하기.java
@@ -0,0 +1,12 @@
+package 프로그래머스.Lv0.나머지_구하기;
+
+/**
+ * 문제 설명
+ * 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+ */
+
+public class 나머지_구하기 {
+	public int solution(int n1, int n2) {
+		return n1%n2;
+	}
+}
