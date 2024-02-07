fd32f22c3bc99f591cabb51ac9951dce1c2e0080
kyu05017 <kyu05017@naver.com>
2024-02-07 19:41:16 +0900

    프로그래머스 Lv0 - 두 수의 곱

---
+++ src/프로그래머스/Lv0/두_수의_곱/두_수의_곱.java
@@ -0,0 +1,14 @@
+package 프로그래머스.Lv0.두_수의_곱;
+
+/**
+ *
+ *  문제 설명
+ *   정수 num1, num2가 매개변수 주어집니다.
+ *   num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+ *
+ */
+public class 두_수의_곱 {
+	public int solution(int a, int b) {
+		return a*b;
+	}
+}
