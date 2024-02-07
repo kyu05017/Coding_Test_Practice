63835c73fcc478f524f36a6d557a0f58e60ef6ec
kyu05017 <kyu05017@naver.com>
2024-02-07 18:17:41 +0900

    프로그래머스 Lv0 - 몫 구하기

---
+++ src/프로그래머스/Lv0/몫_구하기/몫_구하기.java
@@ -0,0 +1,12 @@
+package 프로그래머스.Lv0.몫_구하기;
+
+/**
+ * 문제 설명
+ * 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+ */
+
+public class 몫_구하기 {
+	public int solution(int a, int b) {
+		return a/b;
+	}
+}
