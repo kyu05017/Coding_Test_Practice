11e5ab615651e484dac5ee14339b79d9a747e013
kyu05017 <kyu05017@naver.com>
2024-05-27 20:46:28 +0900

    백준 단계별 문제 풀이 사칙연산

--- src/프로그래머스/Lv0/최빈값_구하기/최빈값_구하기.java
+++ src/프로그래머스/Lv0/최빈값_구하기/최빈값_구하기.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.*;
 
 /**
@@ -54,4 +55,37 @@ class 최빈값_구하기 {
 
         return answer;
     }
+=======
+package 프로그래머스.Lv0.최빈값_구하기;
+
+import java.util.*;
+
+public class 최빈값_구하기 {
+
+	public static int solution(int[] a) {
+		int answer = 0;
+		Map<Integer,Integer> map = new HashMap<>();
+		List<Integer> list = new ArrayList<>();
+		int count  = 0;
+		for(int j : a) {
+			map.put(j, map.getOrDefault(j, 0) + 1);
+		}
+		map.forEach((k,v) ->{
+			list.add(v);
+		});
+
+		list.forEach(e->{
+			out(e+"");
+		});
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		solution(new int[]{1,2,3,3,3,4});
+	}
+	public static void out(String str){
+		System.out.println(str);
+	}
+>>>>>>> 4eca392 (백준 단계별 문제 풀이 사칙연산)
 }
