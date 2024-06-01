d0be6c114a5201560ec0c378fe685b59bd139a57
kyu05017 <kyu05017@naver.com>
2024-06-02 00:03:23 +0900

    백준 단계별 문제 풀이 사칙연산

--- src/백준/입출력과_사칙연산/Bak_01_10952_A_Plus_B_5.java
+++ src/백준/입출력과_사칙연산/Bak_01_10952_A_Plus_B_5.java
@@ -2,7 +2,7 @@ package 백준.입출력과_사칙연산;
 
 import java.util.Scanner;
 
-public class Bak_01_10952_A_Plus_B_5 {
+public class Bak_01_10952_A_Plus_B_5  {
 
 	/**
 	 * 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램 작성
