90013e8a52914e7908679ace4e7874ca20f9a65f
kyu05017 <kyu05017@naver.com>
2024-06-04 19:13:09 +0900

    백준 단계별 문제 풀이 : 조건문 ( 2024.06.04 )

---
+++ src/백준/조건문/Bak_02_14681_사분면_고르기.java
@@ -0,0 +1,31 @@
+package 백준.조건문;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Bak_02_14681_사분면_고르기 {
+
+	public static void main(String[] args)throws IOException {
+		Scanner sc = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int result = 0;
+
+		if(a>0&&b>0){
+			result = 1;
+		}else if(a>0&&b<0){
+			result = 4;
+		}else if(a<0&&b>0){
+			result = 2;
+		}else{
+			result = 3;
+		}
+		bw.write(String.valueOf(result));
+		bw.flush();
+		bw.close();
+	}
+}
