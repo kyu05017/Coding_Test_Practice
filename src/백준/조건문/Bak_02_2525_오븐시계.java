fe041d7ef32fa2ba5c5961db02ac568198603bb2
kyu05017 <kyu05017@naver.com>
2024-06-05 20:20:57 +0900

    백준 단계별 문제 풀이 : 조건문 ( 2024.06.04 )

--- src/백준/조건문/Bak_02_2525_오븐시계.java
+++ src/백준/조건문/Bak_02_2525_오븐시계.java
@@ -1,4 +1,22 @@
 package 백준.조건문;
 
+import java.util.Scanner;
+
 public class Bak_02_2525_오븐시계 {
+
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+
+		String now = sc.nextLine();
+		String[] nowTime = now.split(" ");
+
+		int hour = Integer.parseInt(nowTime[0]);
+		int min = Integer.parseInt(nowTime[1]);
+
+		int target = sc.nextInt();
+
+
+
+	}
 }
