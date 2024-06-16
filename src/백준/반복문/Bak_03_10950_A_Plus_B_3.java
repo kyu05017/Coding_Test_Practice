6639ed88dbc6fab953b1f20e8a0ff03f38f184eb
kyu05017 <kyu05017@naver.com>
2024-06-16 18:25:36 +0900

    백준 단계별 문제 풀이 : 조건문 ( 2024.06.16 ) -A+B_3

--- src/백준/반복문/Bak_03_10950_A_Plus_B_3.java
+++ src/백준/반복문/Bak_03_10950_A_Plus_B_3.java
@@ -1,12 +1,26 @@
 package 백준.반복문;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Bak_03_10950_A_Plus_B_3 {
+public static void main(String[] args)throws IOException {
 
-	public static void main(String[] args)throws IOException {
+		BufferedReader be = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(be.readLine());
+		StringBuilder sb = new StringBuilder();
 
+		int n = Integer.parseInt(st.nextToken());
 
+		for(int i=0;i<n;i++){
+
+			st = new StringTokenizer(be.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			System.out.println(a+b);
+		}
 
 	}
 }
