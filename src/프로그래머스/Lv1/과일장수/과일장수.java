6f185dff27e4ba54d260dd90a8ea2bfd722cf11e
kyu05 <kyu05017@naver.com>
2024-07-29 20:22:29 +0900

    퀌ㄴㄱ

---
+++ src/프로그래머스/Lv1/과일장수/과일장수.java
@@ -0,0 +1,16 @@
+package 프로그래머스.Lv1.과일장수;
+
+public class 과일장수 {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static int sol(int k, int m, int[] score){
+		int result = 0;
+
+		return result;
+	}
+
+
+}
