e28b45d5c95a7988506061f78cd692e50f7141f3
kyu05 <kyu05017@naver.com>
2025-04-27 09:49:31 +0900

    복습및풀이 2025년 04월 27일 오전

--- src/단계별풀이/_25년_04월/복습및풀이_250426/_007_PCCP_체육대회_DFS_복습필요.java
+++ src/단계별풀이/_25년_04월/복습및풀이_250426/_007_PCCP_체육대회_DFS_복습필요.java
@@ -1,10 +1,11 @@
 package 단계별풀이._25년_04월.복습및풀이_250426;
 
+import java.util.*;
+
 public class _007_PCCP_체육대회_DFS_복습필요 {
 	public static void main(String[] args) {
 		System.out.println(solution(new int[][]{{40, 10, 10},{20, 5, 0},{30, 30, 30},{70, 0, 70},{100, 100, 100}}));
 	}
-
 	/**
 	 * 당신이 다니는 학교는 매년 체육대회를 합니다.
 	 * 체육대회는 여러 종목에 대해 각 반의 해당 종목 대표가 1명씩 나와 대결을 하며,
@@ -14,11 +15,64 @@ public class _007_PCCP_체육대회_DFS_복습필요 {
 	 * 당신의 반의 전략은 각 종목 대표의 해당 종목에 대한 능력치의 합을 최대화하는 것입니다.
 	 * 다음은 당신의 반 학생이 5명이고, 종목의 개수가 3개이며, 각 종목에 대한 학생들의 능력치가 아래 표와 같을 때, 각 종목의 대표를 뽑는 예시입니다.
 	 */
+	/// 그리디 + 정렬 => 실패 ( 이유 모름 )
 	private static int solution(int[][] ability){
 		int answer = 0;
+		List<Integer> list = new ArrayList<>();
+		for(int i = 0; i < ability.length; i++){
+			Set<String> set = new HashSet<>();
+			int now_score = ability[i][0];
+			set.add(String.valueOf(i));
 
+			for(int k = 1; k < ability[0].length; k++) {
+				int score = -1;
+				int best_student = -1;
 
+				for (int student = 0; student < ability.length; student++) {
+					if (!set.contains(String.valueOf(student))) {
+						int st_score = ability[student][k];
+						if (score < st_score) {
+							score = st_score;
+							best_student = student;
+						}
+					}
+				}
+				if (best_student != -1) {
+					set.add(String.valueOf(best_student));
+					now_score += score;
+				}
+			}
+			list.add(now_score);
+		}
+		return list.stream().mapToInt(e->e).max().orElse(0);
+	}
 
-		return answer;
+	private static int maxScore = 0;
+	private static boolean[] visited;
+
+	public static int solution1(int[][] ability){
+		int studentCount = ability.length;
+		int eventCount = ability[0].length;
+
+		visited = new boolean[studentCount];
+
+		dfs(0,0,ability,eventCount);
+
+		return maxScore;
+	}
+	static void dfs(int eventIdx, int currentSum, int[][] ability, int totalEvent){
+		if(eventIdx == totalEvent){
+			maxScore = Math.max(maxScore,currentSum);
+			return;
+		}
+
+		for(int student = 0; student < ability.length; student++){
+
+			if(!visited[student]){
+				visited[student] = true;
+				dfs(eventIdx+1,currentSum+ability[student][eventIdx],ability,totalEvent);
+				visited[student] = false;
+			}
+		}
 	}
 }
---
+++ src/단계별풀이/_25년_04월/복습및풀이_250427/_001_PCCE_공원_Lv1.java
@@ -0,0 +1,59 @@
+package 단계별풀이._25년_04월.복습및풀이_250427;
+
+import java.util.Arrays;
+
+public class _001_PCCE_공원_Lv1 {
+	public static void main(String[] args) {
+
+		int[] mats = {5,3,2};
+		String[][] park = {
+				{"A" , "A" , "-1", "B" , "B" , "B" , "B" , "-1"},
+				{"A" , "A" , "-1", "B" , "B" , "B" , "B" , "-1"},
+				{"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"},
+				{"D" , "D" , "-1", "-1", "-1", "-1", "E" , "-1"},
+				{"D" , "D" , "-1", "-1", "-1", "-1", "-1", "F" },
+				{"D" , "D" , "-1", "-1", "-1", "-1", "E" , "-1"}
+		};
+
+		System.out.println("풀이 "+solution(mats,park));
+	}
+	///  완전 탐색
+	private static int solution(int[] mats, String[][] park){
+		int answer = 0;
+
+		// 1. 매트를 큰 순서로 정렬
+		Arrays.sort(mats);
+		int n = mats.length;
+		int[] reverse_arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			reverse_arr[i] = mats[n - i - 1];
+		}
+
+		// 2. 공원 사이즈 구하기
+		int parkSize = park.length; // 정사각형
+
+		// 3. 매트 하나씩 시도
+		for (int size : reverse_arr) {
+			for (int i = 0; i <= parkSize - size; i++) {
+				for (int j = 0; j <= parkSize - size; j++) {
+					if (canPlaceMat(park, i, j, size)) {
+						return size; // 찾으면 바로 리턴
+					}
+				}
+			}
+		}
+
+		return answer;
+	}
+	// 특정 위치에 매트를 놓을 수 있는지 검사
+	private static boolean canPlaceMat(String[][] park, int x, int y, int size) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (!park[x + i][y + j].equals("-1")) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
